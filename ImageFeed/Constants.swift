f7273aa0277b55d07a0e905ecb9f8d0bfc212769
krll-guk <krll.guk@yandex.ru>
2023-03-03 10:21:56 +0300

    Задача по спринту 10

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+let AccessKey = "kYWJD0elwuRLo1IOo2GFeKmOtXqkOTA9ps81yo87xeY"
+let SecretKey = "-YemO2WpJpGZpjhnP3fURT03a63tYKdGeRjrIk2eDY4"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com/")!
